1834ec0f571dad04276ffae1b82489a12fd588b5
Disanda Jayathilaka <disandajayathilaka@gmail.com>
2022-09-08 14:26:14 +0530

    Signup

--- src/App.js
+++ src/App.js
@@ -2,13 +2,21 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import {  useEffect, useState } from 'react';
 import AddTourPackages from "./components/AddTourPackages";
 import Display from "./components/Display";
+import Navbar from "./components/layout/Navbar";
+import Login from "./components/auth/Login";
+import Register from "./components/auth/Register";
+import Landing from "./components/layout/Landing";
 function App() {
+  let isauth = localStorage.getItem('user');
   return (
 
     <div>
         <Router>
+          <Navbar/>
           <Routes>
-
+          <Route exact path="/" element={isauth ? <Display/> : <Landing/>} />
+          <Route  path="/login" element={<Login />} />
+          <Route  path="/register" element={<Register />} />
           <Route  path="/AddTourPackages" element={<AddTourPackages />} />
           <Route  path="/Display" element={<Display />} />
           </Routes>
---
+++ src/components/auth/Login.js
@@ -0,0 +1,85 @@
+import React, { Fragment, useState } from "react";
+import { useNavigate, Link } from "react-router-dom";
+import axios from "axios";
+import Swal from 'sweetalert2';
+
+const Login = () => {
+
+	const navigate = useNavigate();
+
+	const [formData, setFormData] = useState({
+		email: "",
+		password: "",
+	});
+
+	const { email, password } = formData;
+
+	const onChange = (e) => {
+		setFormData({ ...formData, [e.target.name]: e.target.value });
+	}
+
+
+	const onSubmit = async (e) => {
+
+		e.preventDefault();
+		console.log("data",formData)
+		let data = await axios.post("http://localhost:8080/travelgo/user/signin",formData);
+		console.log("data", data?.data);
+		if (data?.data?.userRole) {
+			localStorage.setItem("token", data?.data?.token);
+			localStorage.setItem("userRole", data?.data?.userRole);
+			localStorage.setItem("user", data?.data?.user);
+			navigate("/Display");
+		}
+		else {
+			Swal.fire({
+				icon: 'error',
+				title: 'Oops...',
+				text: 'Login Failed!',
+			})
+		}
+	}
+
+
+	return (
+		<div>
+			<center>
+			<h1 className="heading">Log In</h1>
+			<p className="lead">
+				<i className="fas fa-user"></i> Log Into Your Account
+			</p>
+			<br />
+			<form className="form" onSubmit={(e) => onSubmit(e)}>
+				<div className="form-group">
+					<input
+						type="email"
+						placeholder="Email Address"
+						name="email"
+						value={email}
+						onChange={(e) => onChange(e)}
+						required
+					/>
+				</div>
+				<div className="form-group">
+					<input
+						type="password"
+						placeholder="Password"
+						name="password"
+						minLength="6"
+						value={password}
+						onChange={(e) => onChange(e)}
+						required
+					/>
+				</div>
+				<input type="submit" className="btn btn-warning" value="Login" />
+			</form>
+			<p className="link">
+				Don't have an account? <Link to="/register">Sign Up</Link>
+			</p>
+			</center>
+		</div>
+	);
+};
+
+
+export default Login;
