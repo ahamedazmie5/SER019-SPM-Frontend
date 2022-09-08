64141637b2530878a04cf27976f832f593527cbc
krishmipaboda <krishmipabo@gmail.com>
2022-09-08 11:11:56 +0530

    Frontend changes

--- src/App.js
+++ src/App.js
@@ -2,6 +2,12 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import {  useEffect, useState } from 'react';
 import AddTourPackages from "./components/AddTourPackages";
 import Display from "./components/Display";
+import AddHotelPackages from "./components/AddHotelPackages";
+import ViewHotelDetails from "./components/ViewHotelDetails";
+import UpdateHotelDetails from "./components/UpdateHotelDetails";
+import AddReservation from "./components/AddReservation";
+import UpdateReservation from "./components/UpdateReservation";
+
 function App() {
   return (
 
@@ -11,15 +17,17 @@ function App() {
 
           <Route  path="/AddTourPackages" element={<AddTourPackages />} />
           <Route  path="/Display" element={<Display />} />
+          <Route  path="/AddHotelPackages" element={<AddHotelPackages />} />
+          <Route  path="/ViewHotelDetails" element={<ViewHotelDetails />} />
+          <Route  path="/UpdateHotelDetails/:id" element={<UpdateHotelDetails />} />
+          <Route  path="/AddReservation" element={<AddReservation />} />
+          <Route  path="/UpdateReservation/:id" element={<UpdateReservation />} />
+
           </Routes>
           </Router>
           </div>
 
-
-
-
-
   );
-}//azmie
+}
 
 export default App;
